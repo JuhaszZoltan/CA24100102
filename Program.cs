d81012fdb01410ce449ef46ec7f9f5898ad52664
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2024-10-01 14:04:54 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using CA24100102;
+using System.Text;
+
+List<SportEvent> events = [];
+
+using StreamReader sr = new(
+    path: @"..\..\..\src\paralympics.txt",
+    encoding: Encoding.UTF8);
+_ = sr.ReadLine();
+while (!sr.EndOfStream) events.Add(new(sr.ReadLine()));
+
+Console.WriteLine($"1. feladat: paralimpiak szama: {events.Count}");
---
+++ SportEvent.cs
@@ -0,0 +1,24 @@
+﻿namespace CA24100102
+{
+    internal class SportEvent
+    {
+        public int Year { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public Dictionary<string, int> Medals { get; set; }
+
+        public SportEvent(string row)
+        {
+            var v = row.Split(';');
+            Year = int.Parse(v[0]);
+            Country = v[1];
+            City = v[2];
+            Medals = new()
+            {
+                { "gold", int.Parse(v[3])},
+                { "silver", int.Parse(v[4])},
+                { "bronze", int.Parse(v[5])},
+            };
+        }
+    }
+}
